2976d0cfb3b35a3ba5db5ddcb2f71b9a971509c3
retheviper <youngbina@hotmail.com>
2020-11-29 23:01:45 +0900

    fix: overall

---
+++ src/main/java/global/sesoc/seworld/dto/TableWrapper.java
@@ -0,0 +1,9 @@
+package global.sesoc.seworld.dto;
+
+import java.util.List;
+
+public class TableWrapperDTO {
+	private List<?> aaData;
+	private int iTotalRecords;
+	private int iTotalDisplayRecords;
+}
